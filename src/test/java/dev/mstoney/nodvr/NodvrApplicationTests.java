e1f284af73a5d07e6c05b5963f3c4084f4fb3483
mjstoney <77265862+mjstoney@users.noreply.github.com>
2023-04-05 11:16:16 -0700

    Init Spring Boot with single request mapping

---
+++ src/test/java/dev/mstoney/nodvr/NodvrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mstoney.nodvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
